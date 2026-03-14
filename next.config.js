690f200933697d35147b7f053dbad13829bedf7a
aryvartkrishna <kakuak202@gmail.com>
2026-03-15 03:32:02 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'export', // <-- Cloudflare Pages ke liye ye line sabse zaroori hai
+  eslint: {
+    ignoreDuringBuilds: true, // Agar choti-moti warning hogi toh build fail nahi hone dega
+  },
 };
 
 module.exports = nextConfig;
